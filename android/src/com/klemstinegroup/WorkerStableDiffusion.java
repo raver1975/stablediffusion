7834c36098f3765a63c9ecfc8c29b63d015e6c9d
Paul Klemstine <paulklemstine@gmail.com>
2022-11-09 18:09:56 -0600

    superscale to 2048x2048

--- android/src/com/klemstinegroup/WorkerStableDiffusion.java
+++ android/src/com/klemstinegroup/WorkerStableDiffusion.java
@@ -20,6 +20,7 @@ import com.badlogic.gdx.utils.JsonReader;
 import com.badlogic.gdx.utils.JsonValue;
 
 import java.io.*;
+import java.net.URL;
 import java.util.HashSet;
 import java.util.concurrent.TimeUnit;
 
@@ -183,7 +184,7 @@ public class WorkerStableDiffusion extends Worker {
                         Log.d("prompt", "differential?" + dx + "," + dy);
                         canvas.drawBitmap(srcBmp, dx, dy, null);*/
 
-///*                        //draw inset
+/*                        //draw inset
                         int x = (MAX_AI_WIDTH * xwidth) / xheight;
                         int y = MAX_AI_HEIGHT;
                         Log.d("prompt", x + "," + y + "\t" + "crop");
@@ -193,14 +194,14 @@ public class WorkerStableDiffusion extends Worker {
                         int dx = -(srcBmp.getWidth() - x) / 2;
                         int dy = -(srcBmp.getHeight() - y) / 2;
                         Log.d("prompt", "differential?" + dx + "," + dy);
-                        canvas1.drawBitmap(srcBmp, dx, dy, null);
+                        canvas1.drawBitmap(srcBmp, dx, dy, null);*/
 
-                        //getInpainting(srcBmp, xwidth, xheight, prompt);
+                        getInpainting(srcBmp, xwidth, xheight, prompt);
 
-                        WallpaperManager wallpaperManager = WallpaperManager.getInstance(getApplicationContext());
-                        wallpaperManager.setBitmap(dstBmp1, null, false, WallpaperManager.FLAG_SYSTEM);
-                        wallpaperManager.setBitmap(dstBmp1, null, false, WallpaperManager.FLAG_LOCK);
-                        done = true;
+//                        WallpaperManager wallpaperManager = WallpaperManager.getInstance(getApplicationContext());
+//                        wallpaperManager.setBitmap(dstBmp1, null, false, WallpaperManager.FLAG_SYSTEM);
+//                        wallpaperManager.setBitmap(dstBmp1, null, false, WallpaperManager.FLAG_LOCK);
+//                        done = true;
                     }
 
                 } catch (Exception e) {
@@ -257,11 +258,11 @@ public class WorkerStableDiffusion extends Worker {
     public void getInpainting(Bitmap bitmap, int xwidth, int xheight, String prompt) {
         Log.d("prompt", "getting outpainted image");
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        bitmap.compress(Bitmap.CompressFormat.WEBP, 100, baos);
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, baos);
         byte[] b = baos.toByteArray();
         Log.d("prompt", "byte upload length:" + b.length);
-        String imageEncoded = Base64.encodeToString(b, Base64.NO_WRAP);
-        int x = 2*MAX_AI_HEIGHT;
+        String imageEncoded = "data:image/png;base64,"+Base64.encodeToString(b, Base64.NO_WRAP);  //"data:image/png;base64,"+
+      /*  int x = 2*MAX_AI_HEIGHT;
         int y = 2*MAX_AI_WIDTH;
         if (xheight < xwidth) {
             y = (2*MAX_AI_HEIGHT * xwidth) / xheight;
@@ -269,18 +270,16 @@ public class WorkerStableDiffusion extends Worker {
             x = (2*MAX_AI_WIDTH * xwidth) / xheight;
         }
         x = (x / 64) * 64;
-        y = (y / 64) * 64;
-        Log.d("prompt", "xwidth:" + xwidth + "," + xheight);
-        Log.d("prompt", "asking for size:" + x + "," + y);
+        y = (y / 64) * 64;*/
+//        int x=2*MAX_AI_WIDTH;
+//        int y=2*MAX_AI_HEIGHT;
+//        Log.d("prompt", "xwidth:" + xwidth + "," + xheight);
+//        Log.d("prompt", "asking for size:" + x + "," + y);
         Net.HttpRequest request = new Net.HttpRequest();
-        request.setHeader("apikey", "xfuOtOK5sae3VGX60CJx1Q");
+        request.setHeader("Authorization", "Token 582d29cb9c1594c096c84c1bf7421ba9b97c33a2");
         request.setHeader("Content-Type", "application/json");
-        String outt = "{\"models\":[\"stable_diffusion\"],\"prompt\":\"" + prompt + "\", \"params\":{\"n\":1,\"prompt\":\"" + prompt + "\",\"use_gfpgan\": true, \"karras\": false, \"use_real_esrgan\": true, \"use_ldsr\": true, \"use_upscaling\": true, \"width\": " + x + ", \"height\": " + y + "},\"source_processing\":\"img2img\",\"source_image\":\"" + imageEncoded + "\"}";
-        Log.d("prompt", "out" + outt.substring(0, Math.min(500, outt.length())));
-        request.setContent(outt);
-
-//        request.setContent("{\"source_processing\":\"outpainting\",\"source_image\":\"" + imageEncoded + "\",\"prompt\":\"" + prompt + "\", \"params\":{\"n\":1,\"use_gfpgan\": true, \"karras\": false, \"use_real_esrgan\": true, \"use_ldsr\": true, \"use_upscaling\": true, \"width\": " + x + ", \"height\": " + y + "}}");
-        request.setUrl("https://stablehorde.net/api/v2/generate/sync");
+        request.setContent("{\"version\": \"42fed1c4974146d4d2414e2be2c5277c7fcf05fcc3a73abf41610695738c1d7b\", \"input\": {\"image\": \""+imageEncoded+"\",\"scale\":4,\"face_enhance\":true}}");
+        request.setUrl("https://api.replicate.com/v1/predictions");
         request.setTimeOut(0);
         request.setMethod("POST");
 //        System.out.println("image", "getting image");
@@ -289,68 +288,17 @@ public class WorkerStableDiffusion extends Worker {
             @Override
             public void handleHttpResponse(Net.HttpResponse httpResponse) {
                 String result = httpResponse.getResultAsString();
-                Log.d("prompt", "result" + result.substring(0, Math.min(200, result.length())));
+                Log.d("prompt", "result" + result.substring(0, Math.min(1000, result.length())));
 
 //                Toast.makeText(context, "dreamt of " + prompt, Toast.LENGTH_LONG).show();
                 try {
                     JsonReader reader = new JsonReader();
                     JsonValue resultJSON = reader.parse(result);
-                    JsonValue generations = resultJSON.get("generations");
-                    String imgData = generations.get(0).getString("img");
-                    if (generations != null && imgData != null) {
-                        byte[] bytes = Base64Coder.decode(imgData);
-                        Bitmap srcBmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
-
-                        if (sharedPref.getBoolean("savecheck", false)) {
-                            String filename = "ximage-" + Math.abs(prompt.hashCode()) + "-" + ((int) (Math.random() * Integer.MAX_VALUE)) + ".png";
-                            File sd = Environment.getExternalStoragePublicDirectory(DIRECTORY_DOWNLOADS);
-                            File dest = new File(sd, filename);
-                            try {
-                                FileOutputStream out = new FileOutputStream(dest);
-                                srcBmp.compress(Bitmap.CompressFormat.PNG, 90, out);
-                                out.flush();
-                                out.close();
-                                Log.d("prompt", filename);
-                            } catch (Exception e) {
-                                e.printStackTrace();
-                            }
-                        }
-
-                       /* int x = MAX_AI_HEIGHT;
-                        int y = MAX_AI_WIDTH;
-                        if (xheight > xwidth) {
-                            y = (MAX_AI_HEIGHT * xheight) / xwidth;
-                        } else {
-                            x = (MAX_AI_WIDTH * xheight) / xwidth;
-                        }
-                        Log.d("prompt", "xwidth:" + xwidth + "," + xheight);
-                        Log.d("prompt", "creating size:" + x + "," + y);
-                        Bitmap dstBmp = Bitmap.createBitmap(x, y, Bitmap.Config.ARGB_8888);
-
-                        Canvas canvas = new Canvas(dstBmp);
-                        canvas.drawColor(Color.TRANSPARENT);
-                        int dx = -(srcBmp.getWidth() - x) / 2;
-                        int dy = -(srcBmp.getHeight() - y) / 2;
-                        Log.d("prompt", "differential?" + dx + "," + dy);
-                        canvas.drawBitmap(srcBmp, dx, dy, null);*/
-
-/*                        int x = (2 * MAX_AI_WIDTH * xwidth) / xheight;
-                        int y = 2 * MAX_AI_HEIGHT;
-                        Log.d("prompt", x + "," + y + "\t" + "crop");
-                        Bitmap dstBmp1 = Bitmap.createBitmap(x, y, Bitmap.Config.ARGB_8888);
-                        Canvas canvas1 = new Canvas(dstBmp1);
-                        canvas1.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
-                        int dx = -(srcBmp.getWidth() - x) / 2;
-                        int dy = -(srcBmp.getHeight() - y) / 2;
-                        Log.d("prompt", "differential?" + dx + "," + dy);
-                        canvas1.drawBitmap(srcBmp, dx, dy, null);*/
-
-                        WallpaperManager wallpaperManager = WallpaperManager.getInstance(getApplicationContext());
-                        wallpaperManager.setBitmap(srcBmp, null, false, WallpaperManager.FLAG_SYSTEM);
-                        wallpaperManager.setBitmap(srcBmp, null, false, WallpaperManager.FLAG_LOCK);
-                        done = true;
-                    }
-
+//                    JsonValue generations = resultJSON.get("result");
+                    String id = resultJSON.getString("id");
+                    Log.d("prompt","id:"+id);
+                    getSuperScale(id,xwidth,xheight,prompt);
+                    done = true;
                 } catch (Exception e) {
                     StringWriter sw = new StringWriter();
                     PrintWriter pw = new PrintWriter(sw);
@@ -367,6 +315,98 @@ public class WorkerStableDiffusion extends Worker {
                 t.printStackTrace(pw);
                 Log.d("prompt", sw.toString());
                 done = true;
+            }
+
+            @Override
+            public void cancelled() {
+                Log.d("prompt", "cancelled");
+                done = true;
+            }
+
+        });
+    }
+
+    public void getSuperScale(String id, int xwidth, int xheight,String prompt) {
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        Log.d("prompt", "getting superscaled image");
+        int x=4*MAX_AI_WIDTH;
+        int y=4*MAX_AI_HEIGHT;
+        Log.d("prompt", "xwidth:" + xwidth + "," + xheight);
+        Log.d("prompt", "asking for size:" + x + "," + y);
+        Net.HttpRequest request = new Net.HttpRequest();
+        request.setHeader("Authorization", "Token 582d29cb9c1594c096c84c1bf7421ba9b97c33a2");
+        request.setHeader("Content-Type", "application/json");
+//        request.setContent("{\"version\": \"42fed1c4974146d4d2414e2be2c5277c7fcf05fcc3a73abf41610695738c1d7b\", \"input\": {\"image\": \""+imageEncoded+"\",\"scale\":2,\"face_enhance\":true}}");
+        request.setUrl("https://api.replicate.com/v1/predictions/"+id);
+        request.setTimeOut(0);
+        request.setMethod("GET");
+//        System.out.println("image", "getting image");
+
+        new NetJavaImpl().sendHttpRequest(request, new Net.HttpResponseListener() {
+            @Override
+            public void handleHttpResponse(Net.HttpResponse httpResponse) {
+                String result = httpResponse.getResultAsString();
+                Log.d("prompt", "result" + result.substring(0, Math.min(1000, result.length())));
+
+//                Toast.makeText(context, "dreamt of " + prompt, Toast.LENGTH_LONG).show();
+                try {
+                    JsonReader reader = new JsonReader();
+                    JsonValue resultJSON = reader.parse(result);
+//                    JsonValue generations = resultJSON.get("result");
+                    String id = resultJSON.getString("output");
+                    Log.d("prompt","id:"+id);
+                    URL url = new URL(id);
+                    Bitmap srcBmp = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+                    if (sharedPref.getBoolean("savecheck", false)) {
+                        String filename = "ximage-" + Math.abs(prompt.hashCode()) + "-" + ((int) (Math.random() * Integer.MAX_VALUE)) + ".png";
+                        File sd = Environment.getExternalStoragePublicDirectory(DIRECTORY_DOWNLOADS);
+                        File dest = new File(sd, filename);
+                        try {
+                            FileOutputStream out = new FileOutputStream(dest);
+                            srcBmp.compress(Bitmap.CompressFormat.PNG, 90, out);
+                            out.flush();
+                            out.close();
+                            Log.d("prompt", filename);
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    //draw inset
+                    int x = (MAX_AI_WIDTH * xwidth) / xheight;
+                    int y = MAX_AI_HEIGHT;
+                    Log.d("prompt", x + "," + y + "\t" + "crop");
+                    Bitmap dstBmp1 = Bitmap.createBitmap(x, y, Bitmap.Config.ARGB_8888);
+                    Canvas canvas1 = new Canvas(dstBmp1);
+                    canvas1.drawColor(Color.TRANSPARENT, PorterDuff.Mode.CLEAR);
+                    int dx = -(srcBmp.getWidth() - x) / 2;
+                    int dy = -(srcBmp.getHeight() - y) / 2;
+                    Log.d("prompt", "differential?" + dx + "," + dy);
+                    canvas1.drawBitmap(srcBmp, dx, dy, null);
+                    WallpaperManager wallpaperManager = WallpaperManager.getInstance(getApplicationContext());
+                    wallpaperManager.setBitmap(srcBmp, null, false, WallpaperManager.FLAG_SYSTEM);
+                    wallpaperManager.setBitmap(srcBmp, null, false, WallpaperManager.FLAG_LOCK);
+                    done = true;
+                } catch (Exception e) {
+                    StringWriter sw = new StringWriter();
+                    PrintWriter pw = new PrintWriter(sw);
+                    e.printStackTrace(pw);
+                    Log.d("prompt", sw.toString());
+                    done = true;
+                }
+            }
+
+            @Override
+            public void failed(Throwable t) {
+                StringWriter sw = new StringWriter();
+                PrintWriter pw = new PrintWriter(sw);
+                t.printStackTrace(pw);
+                Log.d("prompt", sw.toString());
+//                done = true;
+                getSuperScale(id,xwidth,xheight,prompt);
             }
 
             @Override
